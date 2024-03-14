3770c7a2f64ce0f0192948a8e31e62cf4d7d93cc
KI JONG BYUN <kjbyun0@gmail.com>
2024-03-14 09:55:46 -0500

    migrated db

--- server/migrations/versions/f8e0a81aaa4d_initial_migration.py
+++ server/migrations/versions/f8e0a81aaa4d_initial_migration.py
@@ -1,8 +1,8 @@
-"""create table birds
+"""initial migration
 
-Revision ID: 6034d52c059f
+Revision ID: f8e0a81aaa4d
 Revises: 
-Create Date: 2022-10-13 16:02:56.583047
+Create Date: 2024-03-14 09:43:13.044191
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6034d52c059f'
+revision = 'f8e0a81aaa4d'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -22,6 +22,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(), nullable=True),
     sa.Column('species', sa.String(), nullable=True),
+    sa.Column('image', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
